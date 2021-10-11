d3b5609ef02ee9ca0dce6dead10707f1916328db
Katharine C Humble <katharinechumble@gmail.com>
2021-10-11 18:20:10 -0400

    added dependencies, installed express, and set up server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+// boilerplate and requiring dependencies
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhose/wild-dunes', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`Connected to localhost: ${PORT}!`));
